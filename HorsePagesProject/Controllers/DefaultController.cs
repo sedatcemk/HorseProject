cfd28746db680ffcb0206865310edf9731f7de39
sedatcemk <kiziltoprak.cem@gmail.com>
2022-11-16 20:00:39 +0100

    fir

---
+++ HorsePagesProject/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HorsePagesProject.Models.Entity;
+
+namespace HorsePagesProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        HorsePagesEntities db = new HorsePagesEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBL_bilgiler.ToList();
+            return View(degerler);
+        }
+    }
+}
---
+++ HorsePagesProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HorsePagesProject.MvcApplication" Language="C#" %>
